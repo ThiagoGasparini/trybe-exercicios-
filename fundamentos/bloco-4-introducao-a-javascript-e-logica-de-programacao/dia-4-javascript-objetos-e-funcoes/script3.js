0adb8175d0612a0a7375c7aeadacf0eef20a68dd
Thiago Gasparini <thiago_engprod@outlook.com>
2022-03-11 10:55:53 -0300

    exercicios for in e objetos finalizados

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script3.js
@@ -0,0 +1,16 @@
+let info = {
+    personagem:'Margarida',
+    origem:'Pato Donald',
+    nota:'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente:'Sim',
+  }
+;
+let info2 = {
+    personagem:'Tio Patinhas',
+    origem:'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota:'O último MacPatinhas',
+    recorrente:'Sim'
+  }
+;
+ 
+console.log(info.personagem + " e " + info2.personagem + '\n' + info.origem + " e " + info2.origem + '\n' + info.nota + ' e ' + info2.nota + '\n' + 'Ambos recorrentes');
