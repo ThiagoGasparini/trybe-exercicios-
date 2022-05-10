c9c728339253573a77bdf9626d3968542d17b3cb
Thiago Gasparini <thiago_engprod@outlook.com>
2022-05-10 17:57:52 -0300

    pokedex

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "./App.css";
-import pokemons from "./data";
+import React from 'react';
+import './App.css';
+import pokemons from './data';
 //Import do json criado com os pokemons e seus dados.
-import Pokedex from "./Pokedex";
+import Pokedex from './Pokedex';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1> Pokedex </h1>
         <Pokedex pokemons={pokemons} />
       </div>
